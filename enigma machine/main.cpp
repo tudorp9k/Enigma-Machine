b701e09e2855ff3891107241b7a04884c05d935a
tudorp9k <tudorp9k@gmail.com>
2022-10-25 18:06:58 +0300

    Changed settings and added input handling

--- enigma machine/main.cpp
+++ enigma machine/main.cpp
@@ -19,21 +19,32 @@ int main()
 
 	// Keyboard & Plugboard
 	Keyboard KB;
-	std::vector<std::string> pairs{ "AR","GK","OX" };
+	std::vector<std::string> pairs{ "ND","AS","KX" };
 	Plugboard PB(pairs);
 
 	// Machine definition
 	Enigma M(B, V, III, IV, PB, KB);
 
 	// Ring settings
-	M.set_rings("NOT");
+	M.set_rings("NYX");
 
 	// Key settings
 	M.set_key("MAN");
 
 	// Message encryption
+	std::string plaintext = "";
 	std::string cyphertext = "";
-	std::string plaintext = { "HELLOWORLD" };
+	std::string input = { "HELLOWORLD" };
+
+	// Input handling
+	for (const auto& c : input)
+	{
+		if (isalpha(c))
+			plaintext.push_back(toupper(c));
+	}
+	plaintext.erase(std::remove_if(plaintext.begin(), plaintext.end(), isspace), plaintext.end());
+
+	// Encryption
 	for (auto const& c : plaintext)
 		cyphertext += M.encipher(c);
 
